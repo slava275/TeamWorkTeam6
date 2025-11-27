b957b387c377752b6a79f604800903fbde25cc24
Anna Kysliak <anyadol79@gmail.com>
2025-11-27 14:11:54 +0200

    Add Anna card

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,10 @@ const Body = () => {
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+              <img src={"Photos/AnnaKysliakPhoto.png"} alt="Anna Kysliak" style={styles.photo} />
         <div style={styles.textContainer}>
-          <h2 style={styles.name}>John Doe</h2>
-          <p style={styles.position}>Software engineer</p>
+          <h2 style={styles.name}>Anna Kysliak</h2>
+          <p style={styles.position}>Progect Manager</p>
         </div>
       </div>
     </main>
